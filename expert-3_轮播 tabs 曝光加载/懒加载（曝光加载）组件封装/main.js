0b76e147867fe5d5a12db74377891f9c8b62f144
ganli <gl09025@gmail.com>
2018-02-07 22:42:09 +0800

    曝光加载，封装

---
+++ expert-3_轮播 tabs 曝光加载/懒加载（曝光加载）组件封装/main.js
@@ -0,0 +1,65 @@
+
+
+
+//立即执行函数进行封装
+Explouser = (function () {
+    function _Explouser($node,callback) {
+        this.$target = $node
+        this.callback = callback
+        var clock
+        this.clock = clock
+        this.bind()
+        this.lazyRender()
+    }
+    //公共方法
+    _Explouser.prototype = {
+        bind: function () {
+            var _this = this
+            $(window).on('scroll', function () {
+                if (_this.clock) {
+                    clearTimeout(_this.clock)
+                }
+                _this.clock = setTimeout(() => {
+                    _this.lazyRender()
+                }, 500);
+            })
+        },
+        lazyRender: function () {
+            if (this.checkShow(this.$target)) {
+                this.callback(this.$target)
+            }
+        },
+        checkShow: function () {
+            var $scrollTop = $(window).scrollTop()
+            var $windowHeight = $(window).height()
+            var $offSet = this.$target.offset().top
+            if ($offSet < $scrollTop + $windowHeight && $offSet > $scrollTop) {
+                return true
+            }
+            return false
+        },
+        loadImg: function ($img) {
+            $img.attr('src', $img.attr('data-src'))
+        }
+    }
+    //第二层封装，返回start函数
+    return {
+        start: function ($ct) {
+            $ct.each(function (index,node) {
+                new _Explouser($(node),function ($node) {
+                    console.log($node)
+                    this.loadImg($node)
+                })
+            })
+        }
+    }
+})()
+
+Explouser.start($('.container img'))
+
+// $('.container img').each(function (index,node) {
+//     new Explouser($(node),function ($node) {
+//         console.log($node)
+//         this.loadImg($node)
+//     })
+// })
